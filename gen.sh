069821aa663dd8fdaa16fa7618f2b2f06cc89027
Sparkvix <ev1lhat0l0ls@gmail.com>
2020-06-06 11:23:11 -0400

    Add null byte detect warning into gen.sh script

--- gen.sh
+++ gen.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-# Simple bash script to generate shellcode
-# $ bash gen.sh file.o
+# Simple bash script to generate shellcode from assembly code
+# > bash gen.sh file.asm
 
-# $ nasm -felf64 execve_sh.nasm -o a.out
+# > nasm -felf64 execve_sh.nasm -o a.out
 
-for i in $(objdump -D $1 -M intel | grep "^ " | cut -f2)
+nasm -felf64 $1 -o a.out && objdump -D -M intel | grep -e " 00" && echo -e "\nShellcode [\033[91mnull byte!\033[0m]:\n" ||
+
+echo -e "Shellcode [\033[92mOK\033[0m]:\n"
+
+for i in $(objdump -D -M intel | grep "^ " | cut -f2)
 do
 	echo -n "\x${i}"
 done
+
+rm a.out
 echo
